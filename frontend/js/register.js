ea3fe152abbf4172ee4a9c59c27807a21b2e4f4c
GustavoBatistaDev <gustavobatistadev@gmail.com>
2023-03-07 20:03:46 -0300

    adicionando requirements

--- frontend/js/register.js
+++ frontend/js/register.js
@@ -1,22 +1,4 @@
-const form = document.querySelector('#form')
 
-    form.addEventListener('submit', event=>{
-        event.preventDefault();
-
-        const formData = new FormData(form);
-        const data = Object.fromEntries(formData);
-        console.log(data)
-
-        fetch('http://127.0.0.1:8000/home/clients/api/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'multipart/form-data', 
-            },
-            body: JSON.stringify(data)
-        })
-    });
-    
-    
     
      
            
