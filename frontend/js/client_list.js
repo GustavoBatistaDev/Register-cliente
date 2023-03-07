430fa9912712eedab06e480f40f767bd9aaee608
GustavoBatistaDev <gustavobatistadev@gmail.com>
2023-03-07 19:16:33 -0300

    volte pra ca

--- frontend/js/client_list.js
+++ frontend/js/client_list.js
@@ -1,23 +1,39 @@
-$.ajax({
-    url: 'http://127.0.0.1:8000/home/clients/api/',
-    method: 'get',
-    async: false,
-    success: function(response){
-        console.log(response)
+async function Clientlist(){
 
-        let container = document.getElementById('content')
-        for(let i in response["results"]){
+    let url = `http://127.0.0.1:8000/home/clients/api/?q=`
 
-            let divCardContainer = document.createElement('div')
+    let form = document.getElementById('form')
+    form.addEventListener('submit', function(e){
+        e.preventDefault();
+        let queryString = document.getElementById('search_id').value
+
+        location.href = `index.html?q=${queryString}`
+        
+    })
+
+    let urlParams = new URLSearchParams(window.location.search)
+    let queryString = urlParams.get("q")
+
+    
+    url = url + queryString
+    console.log(url)
+    const response = await fetch(url)
+
+
+    const data = await response.json();
+
+    let container = document.getElementById('content')
+    for (let i in data["results"]){
+        console.log()
+        let divCardContainer = document.createElement('div')
             divCardContainer.className = "card ml-2  mb-2  sm-2 md-3 lg-4  mt-2 mr-2"
             divCardContainer.style = "background-color: #E8F1F2;width: 22rem; padding: 8px; border-radius: 25px;"
             container.appendChild(divCardContainer)
-            
+
             let cardImg = document.createElement('img')
             cardImg.style = "border-radius: 25px;"
             cardImg.className = "card-img-top cardimg"
-            cardImg.src = response['results'][i]['photo']
-
+            cardImg.src = data['results'][i]['photo']
             divCardContainer.appendChild(cardImg)
 
             let cardBody = document.createElement('div')
@@ -26,29 +42,28 @@ $.ajax({
 
             let h3Card = document.createElement('h3')
             h3Card.className = "card-title title-card"
-            h3Card.innerHTML = response['results'][i]['name']
-            
+            h3Card.innerHTML = data['results'][i]['name']
+
             divCardContainer.append(h3Card)
-           
+
             let pCard = document.createElement('p')
             pCard.className = "card-text pl-3"
-            pCard.innerHTML = 'Registred '+response['results'][i]['created_at']
-            
+            pCard.innerHTML = 'Registred '+data['results'][i]['created_at']
+
             divCardContainer.append(pCard)
 
             let a = document.createElement('a')
             a.id = "card-link"
-           
+
             a.addEventListener('click', get_client) 
             a.innerHTML = 'View more'
             divCardContainer.append(a)
 
             function get_client(){
-                location.href = `detail.html?q=${response['results'][i]['id']}`
+                location.href = `detail.html?q=${data['results'][i]['id']}`}
             }
-            
-        }
     }
-})
+    
 
 
+Clientlist();
